ba661b38b0da883fd25a867df11066c9113cdb34
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-09-20 12:42:07 +0200

    anzeigen der Notes

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -11,44 +11,66 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = []; 
 
-  unsubList;
-  unsubSingle;
-
-  item$;
-  item;
+  unsubTrash;
+  unsubNotes;
 
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-    this.unsubList =  onSnapshot(this.getNotesRef(), (list) => {
-      list.forEach(element => {
-        console.log(element);
-      })
-    });
-
-    this.unsubSingle =  onSnapshot(this.getSingleDocRef("notes", ""), (element) => {
-    });
-
-    this.unsubSingle();
-    this.unsubList();
-
-    this.item$ = collectionData(this.getNotesRef());
-    this.item = this.item$.subscribe((list) => {
-      list.forEach(element => {
-        console.log(element);
-      });
-    });
-    this.item.unsubscribe();
+    this.unsubNotes = this.subNoteList();
+    this.unsubTrash = this.subTrashList();
   }
 
+  ngonDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      })
+    });
+  }
+
+  subNoteList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      })
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || "note",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false
+    }
+  }
+
+  /*
+  * Firestore References für die Notizen
+  */
   getNotesRef(){
     return collection(this.firestore, 'notes');
   }
 
+  /**
+   * Firestore References für den Trash
+   */
   getTrashRef(){
     return collection(this.firestore, 'trash');
   }
 
+  /**
+   * Firestore References für ein einzelnes Dokument
+   */
   getSingleDocRef(colId:string, docID:string) {
     return doc(collection(this.firestore, colId), docID);
   }
